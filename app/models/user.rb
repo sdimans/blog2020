0f1124d86f4ca2c59109209534ce111a782a0a3e
Alexey Alexandrov <aleksandrov1988@gmail.com>
2020-10-05 17:46:20 +0300

    05.10 Half lesson

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, presence: true, uniqueness: true
-  validates :name, presence: true, length: {minimum: 2, maximum: 128}
+  validates :name, presence: true, length: {minimum: 2, maximum: 128  }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :users
   resources :articles
   get 'say/hello'
   get 'say/goodbye'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'say#hello'
+  post 'sessions/create', as: :create_session
 end
