be061f2c669f7e442449b35c7c7776ab51a8cf7b
Alexey Alexandrov <aleksandrov1988@gmail.com>
2020-10-05 18:01:47 +0300

    Login button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to root_path
+    else
+      redirect_to login_path, notice: 'Invalid username or password'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get 'sessions/new', as: :login
   resources :users
   resources :articles
   get 'say/hello'
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'say#hello'
   post 'sessions/create', as: :create_session
+  get 'sessions/destroy', as: :destroy_session
 end
